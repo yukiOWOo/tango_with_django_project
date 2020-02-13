c366f5765250a95ab671f1005e67aa553a711e04
Xunyun jia <2447072xstudent.gla.ac.uk>
2020-02-13 22:39:12 +0000

    C4

--- rango/apps.py
+++ rango/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class RangoConfig(AppConfig):
+class YueConfig(AppConfig):
     name = 'rango'
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,4 +1,4 @@
-"""tango_with_django_project URL Configuration
+"""Long URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name ='about'),
-    path('rango/',include('rango.urls')),
+    path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
-
-]
+    
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
