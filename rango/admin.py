5f38298752131b268e92f6f5508fd6c3dd68ac70
Xunyun jia <2447072xstudent.gla.ac.uk>
2020-02-14 00:05:14 +0000

    C5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+
+class PageAmdin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category)
+admin.site.register(Page, PageAmdin)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,5 +1,4 @@
-"""Long URL Configuration
-
+"""tango_with_django_project URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
@@ -21,9 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('rango/', include('rango.urls')),
-    # The above maps any URLs starting with rango/ to be handled by rango.
-    path('admin/', admin.site.urls),
-    
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+                  path('', views.index, name='index'),
+                  path('rango/', include('rango.urls')),
+                  # The above maps any URLs starting with rango/ to be handled by rango.
+                  path('admin/', admin.site.urls),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
